cd3adc36128b1dca27c0b6f0dede03a350da48ab
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-20 22:17:51 +0100

    Create component ninjas

--- createreactapps/myapp/src/App.js
+++ createreactapps/myapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Ninjas from './ninjas';
 
 
 class App extends Component {
--- createreactapps/myapp/src/ninjas.js
+++ createreactapps/myapp/src/ninjas.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Ninjas extends Component {
-    state = {  }
-    render() { 
-        return ( <div></div> );
+    state = {}
+    render() {
+        return (
+            <div className="Ninjas">
+                <H1>My name is toni</H1>
+                <h4>I am 30 yars old</h4>
+                <h5>I use a black belt</h5>
+            </div>
+        );
     }
 }
- 
+
 export default Ninjas;
