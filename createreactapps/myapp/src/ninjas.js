251b6c99f7e831653db277813cc5c693d311b987
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-22 00:02:03 +0100

    creating a ui component

--- createreactapps/myapp/src/ninjas.js
+++ createreactapps/myapp/src/ninjas.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    state = {}
-    render() {
-        const { ninjas } = this.props
-        var i=0;
-        const ninjaList = ninjas.map(ninja => {
-           
-            ninja.id=i++;
-            return <div key={ninja.id} className="ninjalist">
-                <p>My name is {ninja.name}</p>
-                <p>I am {ninja.age} yars old</p>
-                <p>I use a {ninja.belt} belt</p>
-            </div>
-        })
-        console.log(this.props)
-        return <div className="ninja-list">
+const Ninjas = (props) => {
+    var i = 0;var {ninjas}=props
+    console.log(ninjas)
 
+    const ninjaList = ninjas.map(ninja => {
+
+        ninja.id = i++;
+        return <div key={ninja.id} className="ninjalist">
+            <p>My name is {ninja.name}</p>
+            <p>I am {ninja.age} yars old</p>
+            <p>I use a {ninja.belt} belt</p>
+        </div>
+    })
+
+    return (
+        <div className="ninja-list">
             {ninjaList}
         </div>
-    }
+    )
 }
+
+
+
 
 export default Ninjas;
