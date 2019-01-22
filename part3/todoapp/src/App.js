0ae88c8f4bd746ad53df68a09d5f68f58b469fe7
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-22 01:31:41 +0100

    create react add for todo app and import materialize via cdn

---
+++ part3/todoapp/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+state={
+  todos:[
+    {id:1,content:'I need to buy milk'},
+    {id:2,content:'I need towash cloths'}
+  ]
+}
+
+
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
