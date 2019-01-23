fbed5943c46d9090ec86e1da3ce8f6d1570a7402
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-23 01:58:06 +0100

    Edit navbar

--- part4/i_dont_have_a_name/src/components/navbar.js
+++ part4/i_dont_have_a_name/src/components/navbar.js
@@ -9,6 +9,7 @@ return (
             <ul className="right">
                 <li><Link to="/">Home</Link></li>
                 <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
             </ul>
         </div>
     </nav>
