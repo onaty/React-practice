c71b779d255d722208ff0015b5c488767c339bb2
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-23 01:57:14 +0100

    using react routers

--- part4/i_dont_have_a_name/src/App.js
+++ part4/i_dont_have_a_name/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-
+import Navbar from './components/navbar';
+import { BrowserRouter,Route } from 'react-router-dom'
+import Home from './components/home';
+import About from './components/about';
+import ContactPage from './components/contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
-      </div>
+      
+        <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={ContactPage}/>
+          </div>
+        </BrowserRouter>
+     
     );
   }
 }
