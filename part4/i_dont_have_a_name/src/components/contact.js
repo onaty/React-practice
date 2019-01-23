73d229796c3bbf836cf666dbe80df62e78cf2b95
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-23 01:13:11 +0100

    contact nd navbar

---
+++ part4/i_dont_have_a_name/src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactPage =()=>{
+return (
+    <div className="container">
+        <h4 className="center">ContactPage Page</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin urna mi, semper eu lorem et, convallis commodo erat. Phasellus a massa tempor, imperdiet nulla ac, ullamcorper sem. In non ullamcorper nisi. Aenean mattis vestibulum ante, eget dictum eros fermentum non. Vestibulum mollis ligula turpis, nec luctus magna placerat eu. Curabitur fringilla mollis arcu, fermentum rutrum ligula vulputate non. Duis nunc elit, placerat in leo non, rhoncus congue ipsum. Proin nec placerat ipsum. Vestibulum id ex quis enim pulvinar vulputate ut ac orci. In hac habitasse platea dictumst. </p>
+    </div>
+)
+}
+
+
+export default ContactPage;
